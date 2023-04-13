7800abec63060ef3ff18e974b63de1e5527ca3b9
Asad-Ghouri <asadghouri546@gmail.com>
2023-04-13 09:16:06 -0700

    .

--- pages/swap.tsx
+++ pages/swap.tsx
@@ -56,13 +56,13 @@ export default function MainSwap() {
             frameBorder="0"
             id="swap_iframe"
             allow="clipboard-read *; clipboard-write *; web-share *; accelerometer *; autoplay *; camera *; gyroscope *; payment *; geolocation *"
-            src="https://flooz.trade/embed/trade?swapDisabled=false&swapToTokenAddress=0x62897b455B97e573f1674661406722Dc5bF01003&swapLockToToken=true&onRampDisabled=false&onRampAsDefault=false&onRampDefaultAmount=200&onRampTokenAddress=0x62897b455B97e573f1674661406722Dc5bF01003&network=bsc&lightMode=false&primaryColor=%23ff6600&roundedCorners=10&padding=20&refId=PjKSWG"
+            src="https://flooz.trade/embed/trade?swapDisabled=false&swapToTokenAddress=0x834619439294Beb318e1b73Be3D20d2b581D4bFb&swapLockToToken=true&onRampDisabled=false&onRampAsDefault=false&onRampDefaultAmount=200&onRampTokenAddress=0x834619439294Beb318e1b73Be3D20d2b581D4bFb&network=bsc&lightMode=false&primaryColor=%23ff6600&roundedCorners=10&padding=20&refId=PjKSWG"
           ></iframe>
           <center>
             <h1 className="swap-text" style={{ marginTop: "7rem" }}>Purchase</h1>
           </center>
 
-          <iframe height={625} title="Transak On/Off Ramp Widget" src="https://global.transak.com?apiKey=69d69a4f-c43b-4077-a3b2-32f82c7a5a46" frameBorder="no" allowFullScreen style={{ display: 'block', width: '100%', maxHeight: '625px', maxWidth: '400px', margin: 'auto', marginTop: "2rem" }}>
+          <iframe height={625} id="purchase_iframe" title="Transak On/Off Ramp Widget" src="https://global.transak.com?apiKey=69d69a4f-c43b-4077-a3b2-32f82c7a5a46" frameBorder="no" allowFullScreen style={{ display: 'block', width: '100%', maxHeight: '625px', maxWidth: '400px', margin: 'auto', marginTop: "2rem" }}>
           </iframe>
 
           <div className="_container">
